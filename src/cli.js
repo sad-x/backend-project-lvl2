e701dfc750ba38f15d3fb2c388935430c568a897
alex s <alex.shamess@yandex.ru>
2021-08-27 17:16:27 +0300

    added .npmrc

---
+++ src/cli.js
@@ -0,0 +1,15 @@
+import { program } from 'commander';
+import genDiff from './flat-json-diff.js';
+
+export default () => {
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log(genDiff(filepath1, filepath2));
+    })
+    .parse();
+};
