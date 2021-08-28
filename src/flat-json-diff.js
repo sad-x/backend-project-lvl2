eebd613644825e3c6b9aa3dc0bffbd83a36399dc
alex s <alex.shamess@yandex.ru>
2021-08-28 18:24:02 +0300

    refactored flet-json-diff.js

--- src/flat-json-diff.js
+++ src/flat-json-diff.js
@@ -2,50 +2,54 @@ import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
-const getObjectFromFile = (pathToFile) => {
-  const file = path.resolve(process.cwd(), pathToFile);
-  const obj = JSON.parse(fs.readFileSync(file, 'utf8'));
-  return obj;
+const readFile = (filename) => {
+  const filepath = path.resolve(process.cwd(), filename);
+  const content = fs.readFileSync(filepath, 'utf8');
+  return content;
 };
 
-const getDifferences = (obj1, obj2) => {
+const parse = (content) => JSON.parse(content);
+
+const makeLeaf = (key, oldValue, newValue) => ({ key, oldValue, newValue });
+
+const makeTree = (obj1, obj2) => {
   const entries1 = Object.entries(obj1);
   const entries2 = Object.entries(obj2);
 
   const onlyInFirst = entries1
     .filter(([key]) => entries2.every(([key2]) => key !== key2))
-    .map(([key, value]) => ({ key, oldValue: value, newValue: undefined }));
+    .map(([key, value]) => makeLeaf(key, value, undefined));
   const onlyInSecond = entries2
     .filter(([key]) => entries1.every(([key2]) => key !== key2))
-    .map(([key, value]) => ({ key, oldValue: undefined, newValue: value }));
+    .map(([key, value]) => makeLeaf(key, undefined, value));
   const sameItems = entries1
     .filter(([key]) => entries2.some(([key2]) => key === key2))
-    .map(([key, value]) => {
-      const [, newValue] = entries2.find(([key2]) => key2 === key);
-      return ({ key, oldValue: value, newValue });
-    });
+    .map(([key, value]) => makeLeaf(key, value, obj2[key]));
 
-  console.log(sameItems);
   return [...onlyInFirst, ...onlyInSecond, ...sameItems];
 };
 
-const makeLeaf = (key, oldValue, newValue) => {
+// на вход лист
+const formatLeaf = (leaf) => {
+  const { key, oldValue, newValue } = leaf;
   if (oldValue === undefined) return `  + ${key}: ${newValue}`;
   if (newValue === undefined) return `  - ${key}: ${oldValue}`;
   if (oldValue === newValue) return `    ${key}: ${newValue}`;
   return `  - ${key}: ${oldValue}\n  + ${key}: ${newValue}`;
 };
 
-const sortDifferences = (differences) => {
-  const sortedDiffs = _.orderBy(differences, ['key'], ['asc']);
-  const joinedLeafs = sortedDiffs.map(({ key, oldValue, newValue }) => makeLeaf(key, oldValue, newValue)).join('\n');
-  return `{\n${joinedLeafs}\n}`;
+const formatTree = (tree) => {
+  const sortedDiffs = _.orderBy(tree, ['key'], ['asc']);
+  const joinedLeaves = sortedDiffs.map((leaf) => formatLeaf(leaf)).join('\n');
+  return `{\n${joinedLeaves}\n}`;
 };
 
 const genDiff = (path1, path2) => {
-  const obj1 = getObjectFromFile(path1);
-  const obj2 = getObjectFromFile(path2);
-  return sortDifferences(getDifferences(obj1, obj2));
+  const content1 = readFile(path1);
+  const content2 = readFile(path2);
+  const obj1 = parse(content1);
+  const obj2 = parse(content2);
+  return formatTree(makeTree(obj1, obj2));
 };
 
 export default genDiff;
