813d82baaef5a507359bbe06dc706f2750a20af8
alex s <alex.shamess@yandex.ru>
2021-08-28 15:22:10 +0300

    refactored flat-json-diff

--- src/flat-json-diff.js
+++ src/flat-json-diff.js
@@ -1,4 +1,4 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 
@@ -9,51 +9,37 @@ const getObjectFromFile = (pathToFile) => {
 };
 
 const getDifferences = (obj1, obj2) => {
-  const arr1 = Object.entries(obj1);
-  const arr2 = Object.entries(obj2);
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const values1 = Object.values(obj1);
-  const values2 = Object.values(obj2);
+  const entries1 = Object.entries(obj1);
+  const entries2 = Object.entries(obj2);
 
-  // status 0: ' ', 1: '-', 2: '+'
+  const onlyInFirst = entries1
+    .filter(([key]) => entries2.every(([key2]) => key !== key2))
+    .map(([key, value]) => ({ key, oldValue: value, newValue: undefined }));
+  const onlyInSecond = entries2
+    .filter(([key]) => entries1.every(([key2]) => key !== key2))
+    .map(([key, value]) => ({ key, oldValue: undefined, newValue: value }));
+  const sameItems = entries1
+    .filter(([key]) => entries2.some(([key2]) => key === key2))
+    .map(([key, value]) => {
+      const [, newValue] = entries2.find(([key2]) => key2 === key);
+      return ({ key, oldValue: value, newValue });
+    });
 
-  const onlyInFirst = arr1
-    .filter((item) => !keys2.includes(item[0]))
-    .map((item) => ({ key: item[0], value: item[1], status: 1 }));
-  const onlyInSecond = arr2
-    .filter((item) => !keys1.includes(item[0]))
-    .map((item) => ({ key: item[0], value: item[1], status: 2 }));
-  const sameItems = arr1
-    .filter((item) => keys2.includes(item[0]) && values2.includes(item[1]))
-    .map((item) => ({ key: item[0], value: item[1], status: 0 }));
-  const notSameItems1 = arr1
-    .filter((item) => keys2.includes(item[0]) && !values2.includes(item[1]))
-    .map((item) => ({ key: item[0], value: item[1], status: 1 }));
-  const notSameItems2 = arr2
-    .filter((item) => keys1.includes(item[0]) && !values1.includes(item[1]))
-    .map((item) => ({ key: item[0], value: item[1], status: 2 }));
-
-  const all = [...onlyInFirst, ...onlyInSecond, ...sameItems, ...notSameItems1, ...notSameItems2];
-  return all;
+  console.log(sameItems);
+  return [...onlyInFirst, ...onlyInSecond, ...sameItems];
 };
 
-const getStatus = (status) => {
-  if (status === 0) return '    ';
-  if (status === 1) return '  - ';
-  return '  + ';
+const makeLeaf = (key, oldValue, newValue) => {
+  if (oldValue === undefined) return `  + ${key}: ${newValue}`;
+  if (newValue === undefined) return `  - ${key}: ${oldValue}`;
+  if (oldValue === newValue) return `    ${key}: ${newValue}`;
+  return `  - ${key}: ${oldValue}\n  + ${key}: ${newValue}`;
 };
 
 const sortDifferences = (differences) => {
-  const sortedDiffs = _.orderBy(differences, ['key', 'status'], ['asc', 'asc']);
-  const result = sortedDiffs.reduce((acc, item) => {
-    const status = getStatus(item.status);
-    const str = status.concat(item.key, ': ', item.value);
-    return acc.concat(str, '\n');
-  }, '');
-  const res = result.concat('}');
-  const r = '{\n'.concat(res);
-  return r;
+  const sortedDiffs = _.orderBy(differences, ['key'], ['asc']);
+  const joinedLeafs = sortedDiffs.map(({ key, oldValue, newValue }) => makeLeaf(key, oldValue, newValue)).join('\n');
+  return `{\n${joinedLeafs}\n}`;
 };
 
 const genDiff = (path1, path2) => {
