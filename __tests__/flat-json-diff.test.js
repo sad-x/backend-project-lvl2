72af630bc586c7f4fb8c5ad2dfc70a58cf87d043
alex s <alex.shamess@yandex.ru>
2021-08-30 12:35:49 +0300

    some changes

--- __tests__/flat-json-diff.test.js
+++ __tests__/flat-json-diff.test.js
@@ -3,7 +3,7 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import { readFileSync } from 'fs';
-import genDiff from '../src/flat-json-diff.js';
+import genDiff from '../src/flat-diff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
