e41593b3228431317c9ee94e88fcf876fa0dd343
alex s <alex.shamess@yandex.ru>
2021-08-30 12:39:45 +0300

    modified parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,11 @@
 import yaml from 'js-yaml';
 import path from 'path';
 
+// eslint-disable-next-line consistent-return
 const getParser = (filePath) => {
   const ext = path.extname(filePath);
   if (ext === '.json') return JSON.parse;
-  return yaml.load;
+  if (ext === '.yml' || ext === '.yaml') return yaml.load;
 };
 
 export default getParser;
