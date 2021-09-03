9d93ae94e6de2a007429f07b6f14cb515b5b2426
alex s <alex.shamess@yandex.ru>
2021-09-03 15:58:46 +0300

    optimized code for work with new functionality

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,12 +10,9 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test.each([
-  ['json'],
-  ['yml'],
-])('genDiff %s', (extention) => {
-  const input1Path = getFixturePath(`file1.${extention}`);
-  const input2Path = getFixturePath(`file2.${extention}`);
+test('genDiff', () => {
+  const input1Path = getFixturePath('file1-nest.json');
+  const input2Path = getFixturePath('file2-nest.json');
   const outputPath = getFixturePath('output.txt');
   const expected = readFileSync(outputPath, 'utf-8');
   expect(genDiff(input1Path, input2Path)).toEqual(expected);
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
 import fs from 'fs';
 import _ from 'lodash';
 import path from 'path';
 import getParser from './parsers.js';
+import makeTree from './tree.js';
 
 const readFile = (filename) => {
   const filepath = path.resolve(process.cwd(), filename);
@@ -9,48 +12,39 @@ const readFile = (filename) => {
   return content;
 };
 
-const makeLeaf = (key, oldValue, newValue) => ({ key, oldValue, newValue });
-
-const makeTree = (obj1, obj2) => {
-  const entries1 = Object.entries(obj1);
-  const entries2 = Object.entries(obj2);
-
-  const onlyInFirst = entries1
-    .filter(([key]) => entries2.every(([key2]) => key !== key2))
-    .map(([key, value]) => makeLeaf(key, value, undefined));
-  const onlyInSecond = entries2
-    .filter(([key]) => entries1.every(([key2]) => key !== key2))
-    .map(([key, value]) => makeLeaf(key, undefined, value));
-  const sameItems = entries1
-    .filter(([key]) => entries2.some(([key2]) => key === key2))
-    .map(([key, value]) => makeLeaf(key, value, obj2[key]));
-
-  return [...onlyInFirst, ...onlyInSecond, ...sameItems];
-};
-
-const formatLeaf = (leaf) => {
-  const { key, oldValue, newValue } = leaf;
-  if (oldValue === undefined) return `  + ${key}: ${newValue}`;
-  if (newValue === undefined) return `  - ${key}: ${oldValue}`;
-  if (oldValue === newValue) return `    ${key}: ${newValue}`;
-  return `  - ${key}: ${oldValue}\n  + ${key}: ${newValue}`;
-};
-
 const formatTree = (tree) => {
   const sortedDiffs = _.orderBy(tree, ['key'], ['asc']);
   const joinedLeaves = sortedDiffs.map((leaf) => formatLeaf(leaf)).join('\n');
-  return `{\n${joinedLeaves}\n}`;
+  return `${joinedLeaves}`;
 };
 
+// json.stringify()
+const formatLeaf = (leaf) => {
+  const {
+    key, oldValue, newValue, level, type, children,
+  } = leaf;
+  const tabs = level === 1 ? '    ' : '  '.repeat(level * 2 - 1);
+  if ((_.isObject(oldValue) || _.isObject(newValue)) && type === 'leaf') {
+    const data = newValue === undefined ? oldValue : newValue;
+  }
+  if (type === 'branch') return `${tabs}${key} {\n${formatTree(children)}\n${tabs}}`;
+  if (oldValue === undefined) return `${tabs}+ ${key}: ${newValue}`;
+  if (newValue === undefined) return `${tabs}- ${key}: ${oldValue}`;
+  if (oldValue === newValue) return `${tabs}  ${key}: ${newValue}`;
+  return `${tabs}- ${key}: ${oldValue}\n${tabs}+ ${key}: ${newValue}`;
+};
+
+// getFormater()
 const genDiff = (path1, path2) => {
   const parse = getParser(path1);
   const content1 = readFile(path1);
   const content2 = readFile(path2);
   const obj1 = parse(content1);
   const obj2 = parse(content2);
-  return formatTree(makeTree(obj1, obj2));
+  const tree = makeTree(obj1, obj2, 1);
+  return formatTree(tree);
 };
 
-// console.log(genDiff('__fixtures__/file1-nest.json', '__fixtures__/file2-nest.json'));
+console.log(genDiff('__fixtures__/file1-nest.json', '__fixtures__/file2-nest.json'));
 
 export default genDiff;
